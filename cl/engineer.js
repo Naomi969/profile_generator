fbfefe4c199b0853a1c63be79cc369d51421d069
Naomi <nani.torres.serrano@outlook.com>
2021-06-08 11:37:34 -0400

    finished engineer

--- cl/engineer.js
+++ cl/engineer.js
@@ -1,7 +1,17 @@
 const employee = require('./employee');
 
 class engineer extends employee {
-    constructor (emName, id, email, github) {
+    constructor(emName, id, email, github) {
         super(emName, id, email)
+        this.github = github
     }
+
+    getGithub() {
+
+    }
+    getRole() {
+        return 'engineer'
+    }
+
 }
+module.exports = engineer;
