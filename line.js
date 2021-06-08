28ac7a5eafa8f0252499689d83e060abe980be3e
Naomi <nani.torres.serrano@outlook.com>
2021-06-08 12:02:15 -0400

    addes

--- line.js
+++ line.js
@@ -1,3 +1,7 @@
 const inquirer = require('inquirer');
 const fs = require('fs') ;
 
+const engineer = require('./cl/engineer');
+const intern = require('./cl/intern');
+const manager = require('./cl/manager');
+
